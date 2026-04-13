d05dab01634defd2075ee8270cd20c1e9dca5edb
Igor <josedupagghg@gmail.com>
2026-04-12 19:43:54 -0500

    PromediosE

--- Calificaciones.c
+++ Calificaciones.c
@@ -20,6 +20,27 @@ int main() {
             } while (calificaciones[i][j] < 0 || calificaciones[i][j] > 10);
         }
     }
+
+    // Calcular promedios por estudiante
+    for (int i = 0; i < ESTUDIANTES; i++) {
+        float suma = 0;
+        maxEstudiante[i] = calificaciones[i][0];
+        minEstudiante[i] = calificaciones[i][0];
+        for (int j = 0; j < ASIGNATURAS; j++) {
+            suma += calificaciones[i][j];
+            if (calificaciones[i][j] > maxEstudiante[i]) maxEstudiante[i] = calificaciones[i][j];
+            if (calificaciones[i][j] < minEstudiante[i]) minEstudiante[i] = calificaciones[i][j];
+        }
+        promedioEstudiante[i] = suma / ASIGNATURAS;
+    }
+
+    // Mostrar resultados
+    printf("\n--- Resultados ---\n");
+    for (int i = 0; i < ESTUDIANTES; i++) {
+        printf("Estudiante %d -> Promedio: %.2f | Max: %.2f | Min: %.2f\n",
+               i+1, promedioEstudiante[i], maxEstudiante[i], minEstudiante[i]);
+    }
+
    
     return 0;
 }
